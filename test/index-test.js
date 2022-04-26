aa423859dbeb17e805f6411d2cfcb9e78d4bf47b
Trevon Mays <mays.trevon94@gmail.com>
2022-04-26 10:23:17 -0700

    commit tree 798981c5663a637d7ce7cfdc4f77e8622f78bef6

--- test/index-test.js
+++ test/index-test.js
@@ -1,10 +1,5 @@
-const jsdom = require( 'jsdom' );
-const path = require( 'path' );
-var chai = require( 'chai' );
-chai.use( require( 'chai-fs' ) );
-
-describe( 'This assignment', () => {
-  it( 'has been correctly cloned to your local environment', () => {
-    chai.assert.isDirectory( './.git', 'no ".git" folder was found within "welcome-completing-assignment". Use "git init" to create one' );
-  } )
-} )
+const name = "Joe"
+const height = 74
+const message = `${name} is ${height} inches tall`
+module.exports = { name, height, message }
+console.log(message)
